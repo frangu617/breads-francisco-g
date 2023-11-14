e4b0c25a932d326dc97377a019fa94248d074c2d
frangu617 <142462584+frangu617@users.noreply.github.com>
2023-11-13 20:04:57 -0800

    sending index and show data

--- server.js
+++ server.js
@@ -11,10 +11,16 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('.jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
 
 //ROUTES
 app.get('/', (req, res) => {
     res.send(`<h1>Welcome to an Awesome App about <a href = '/breads'>Bread!</a></h1>`)
+})
+
+app.get('*', (req, res) =>{
+    res.send(`404 <img src = "https://http.cat/404"/>`)
 })
 
 //BREADS
--- views/index.jsx
+++ views/index.jsx
@@ -1,41 +1,17 @@
 const React = require("react");
 const Default = require("./layouts/default");
 
-
 function Index({ breads, title }) {
-  const borderStyle = {
-    border: "2px solid #000", // Define border properties here
-    width: "400px",
-    textAlign: "center",
-    backgroundColor: "green",
-    marginBottom: "20px",
-    paddingBottom: "10px",
-  };
-  const anchorStyle = {
-    color: "white",
-    textDecoration: "none",
-  };
-  const listStyle = {
-    listStyleType: "none",
-  };
-
-  
-
   return (
     <Default title={title}>
       <h2>Index Page</h2>
       {/* This is a JSX comment */}
       {/* <p>I have {breads[0].name} bread!</p> */}
-      <ul style={listStyle}>
+      <ul>
         {breads.map((bread, index) => {
           return (
-            
-            <li key={index} style={borderStyle}>
-              
-              <a href={`/breads/${index}`} style = {anchorStyle}>
-                <h1>{bread.name}</h1>
-                <img src={bread.image} height={200}></img>
-              </a>
+            <li key={index}>
+              <a href={`/breads/${index}`}>{bread.name}</a>
             </li>
           );
         })}
